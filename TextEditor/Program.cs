679f806c5d2100e3846a77a295b19b605b0e08a6
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-02-26 13:02:00 -0300

    Text Editor with error

---
+++ TextEditor/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace TextEditor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu");
+            Console.WriteLine("0 - Exit program");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Create/Edit file");
+            short s = short.Parse(Console.ReadLine());
+
+            switch(s)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    ReadFile();
+                    break;
+                case 2:
+                    EditFile();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+
+        static void ReadFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Path of the file: ");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void EditFile()
+        {
+            Console.Clear();
+            Console.WriteLine("Digit the text below (ESC to exit)");
+            Console.WriteLine("==========");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+            System.Console.WriteLine(text);
+            Console.ReadKey();
+            Save(text);
+        }
+        
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Which path to save the file?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine("File Saved");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
