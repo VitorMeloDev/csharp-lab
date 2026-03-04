a56150522bd0d4e1dddb15066a27fd1803bc3820
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-03-04 14:39:54 -0300

    Update ReviewMenu.cs: Create ReadMultilineInput

--- Portal_Games/Content/Review.cs
+++ Portal_Games/Content/Review.cs
@@ -2,9 +2,21 @@ namespace Portal.Content
 {
     class Review
     {
+        public Guid Id {get; set;}
         public string Title {get; set;}
         public string Subtitle {get; set;}
         public string GameReview {get; set;}
         public DateOnly PublicationDate {get; set;}
+
+        public Review(){}
+
+        public Review(string title, string subtitle, string review)
+        {
+            Id = Guid.NewGuid();
+            PublicationDate = DateOnly.FromDateTime(DateTime.Today);
+            Title = title;
+            Subtitle = subtitle;
+            GameReview = review;
+        }
     }
 }
--- Portal_Games/Screens/ReviewMenu.cs
+++ Portal_Games/Screens/ReviewMenu.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using Portal.Content;
 
 namespace Portal.Screens
 {
@@ -6,7 +7,48 @@ namespace Portal.Screens
     {
         public static void Init()
         {
-            
+            Console.Clear();
+            Console.WriteLine("Review Menu\n0 - Main menu\n1 - Escrever Resenha\n2 - Ler resenha");
+            short x = short.Parse(Console.ReadLine());
+
+            switch(x)
+            {
+                case 0:
+                    MainMenu.Init();
+                    break;
+                default: 
+                    Init();
+                    break;
+            }
+        }
+
+        public static void WriteReview()
+        {
+            string title = Console.ReadLine();
+            string subtitle = Console.ReadLine();
+            string review = ReadMultilineInput();
+
+            Review gameReview = new Review(title, subtitle, review);
+        }
+
+        public static string ReadMultilineInput()
+        {
+            Console.WriteLine("Write the review (type END on a new line to finish):");
+
+            var lines = new List<string>();
+
+            while(true)
+            {
+                string? line = Console.ReadLine();
+
+                if(line == null)
+                    break;
+                if(line.ToUpper() == "END")
+                    break;
+                lines.Add(line);
+            }
+
+            return string.Join(Environment.NewLine, lines);
         }
     }
 }
